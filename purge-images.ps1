089e6f8a9a62282331d174496cdcfac088419eaa
Navid-Radkusha <45106824+navPersia@users.noreply.github.com>
2025-09-19 14:01:43 +0200

    Update purge-images.ps1

--- purge-images.ps1
+++ purge-images.ps1
@@ -5,8 +5,9 @@ $acrFqdn = "$($acrName).azurecr.io"
 $aksName = "aks-sharedservices-dev-01"
 $aksResourceGroup = "rg-sharedservices-dev-01"
 
+# ToDo: login on az cli
+# ToDo: login on az cli using system managed identity
 az login --identity
-
 # Get AKS credentials. Use --admin if your MI/SP lacks cluster user role powershell
 az aks get-credentials --overwrite-existing --name $aksName --resource-group $aksResourceGroup
 # ToDo: get k8s pairs
@@ -36,10 +37,17 @@ $k8sPairs = kubectl get pods -A -o json |
 
 $k8sPairs | Format-Table -AutoSize
 
-
 $allTop4Tags = @() # Top4 tags
 $allTags = @() # All tags
-$acrImages = az acr repository list --name $acrName --output tsv
+# ToDo: retry logic for $acrImages
+try {
+  $acrImages = az acr repository list --name $acrName --output tsv
+} catch {
+  Write-Warning "Error getting acr images. Retrying..."
+  az login --identity
+  $acrImages = az acr repository list --name $acrName --output tsv
+}
+
 foreach ($repo in $acrImages) {
     $top4 = az acr repository show-tags --name $acrName --repository $repo --orderby time_asc --top 4 --output tsv
     $all = az acr repository show-tags --name $acrName --repository $repo --output tsv
@@ -78,3 +86,9 @@ foreach ($tag in $allTags) {
 $unusedTags = $unusedTags | Where-Object { $_ -ne "pwsh-purge-task:latest" }
 # log the unusedTags
 $unusedTags | Format-Table -AutoSize
+
+# # ToDo: delelte the unusedTags from the acr
+foreach ($unusedTag in $unusedTags) {
+    az acr repository delete --name $acrName --image $unusedTag --yes
+    Write-Host "Deleted $unusedTag"
+}
